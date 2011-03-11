2f5d201257cbc6fcfac71e52bdfc63572eba6ec9
Antti Virtanen <antti.i.virtanen@nokia.com>
2011-03-11 15:39:56 +0200

    Fixed proximitydata comparison check

--- sensors/proximitysensor/proximitysensor.cpp
+++ sensors/proximitysensor/proximitysensor.cpp
@@ -110,8 +110,11 @@ void ProximitySensorChannel::emitData(const ProximityData& value)
 {
     previousValue_.timestamp_ = value.timestamp_;
 
-    if (value.value_ != previousValue_.value_) {
+    if (value.value_ != previousValue_.value_ ||
+        value.withinProximity_ != previousValue_.withinProximity_)
+    {
         previousValue_.value_ = value.value_;
+        previousValue_.withinProximity_ = value.withinProximity_;
         writeToClients((const void *)&value, sizeof(ProximityData));
     }
 }
