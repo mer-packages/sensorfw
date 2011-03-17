7c95896fad689ffce274549fe34d21e0e2d8b856
Antti Virtanen <antti.i.virtanen@nokia.com>
2011-03-17 15:35:58 +0200

    Fixed crash bug from qtmob testapp. Updated testapp default config to more reasonable.

--- tests/testapp/sensorhandler_qtmob.cpp
+++ tests/testapp/sensorhandler_qtmob.cpp
@@ -81,7 +81,7 @@ SensorHandler::SensorHandler(const QString& sensorName, QObject *parent) :
 
     m_sensor->setProperty("alwaysOn", standbyoverride_);
     m_sensor->setProperty("bufferSize", buffersize_);
-    m_sensor->setDataRate(1000 / interval_);
+    m_sensor->setDataRate(interval_ ? 1000 / interval_ : 0);
 }
 
 SensorHandler::~SensorHandler()
